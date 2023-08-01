6498d4df8bb047c4892f7ad5f114c8a1bb263726
Spencer H <SpencerHanson3141@gmail.com>
2023-08-01 16:09:45 -0600

    initial package structure

---
+++ tests/run_dev_testing.py
@@ -0,0 +1,33 @@
+import tempfile
+
+from ecephys_spike_sorting.scripts.create_input_json import createInputJson
+import os
+import subprocess
+"""
+Code Sourced from https://github.com/AllenInstitute/ecephys_spike_sorting/blob/master/ecephys_spike_sorting/scripts/batch_processing.py
+Adapted to fit needs
+"""
+
+
+def generate_input_json():
+    # createInputJson(output_file,
+    #                 npx_directory=None,
+    #                 continuous_file=None,
+    #                 extracted_data_directory=None,
+    #                 kilosort_output_directory=None,
+    #                 kilosort_output_tmp=None,
+    #                 probe_type='3A')
+
+    tmpfile = tempfile.NamedTemporaryFile(delete=False)
+    json_data = createInputJson(tmpfile.name, extracted_data_directory="E:\\NeuroPixelsTest")
+    tw = 2
+    return json_data
+    pass
+
+
+def main():
+    config = generate_input_json()
+
+
+if __name__ == "__main__":
+    main()
