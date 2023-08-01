b934808f0b91ea71493bfe819d2db07f04ec4dc1
Spencer H <SpencerHanson3141@gmail.com>
2023-08-01 17:20:59 -0600

    update dev testing

--- tests/run_dev_testing.py
+++ tests/run_dev_testing.py
@@ -1,7 +1,14 @@
+import json
 import tempfile
 import time
+import uuid
+
 import numpy as np
 import os
+
+# Suppress Git error when importing from ecephys, so it doesn't throw an exception
+os.environ["GIT_PYTHON_REFRESH"] = "quiet"
+
 from ecephys_spike_sorting.scripts.create_input_json import createInputJson
 from ecephys_spike_sorting.common.utils import load_kilosort_data
 from ecephys_spike_sorting.modules.quality_metrics.metrics import calculate_metrics
@@ -15,22 +22,29 @@ Adapted to fit needs
 """
 
 
-def run_quality_metrics(kilosort_output_directory, sample_rate, quality_metrics_params):
+def run_quality_metrics(kilosort_output_directory, sample_rate, quality_metrics_params, save_to_file=None, calc_pcs=True):
+    if save_to_file is None and not isinstance(save_to_file, str):
+        raise ValueError("Error, when specifying 'save_to_file', value must be a string!")
+
     print('ecephys spike sorting: quality metrics module')
     start = time.time()
 
     print("Loading data...")
     try:
-        spike_times, spike_clusters, spike_templates, amplitudes, templates, channel_map, clusterIDs, cluster_quality, pc_features, pc_feature_ind = \
-            load_kilosort_data(
-                kilosort_output_directory,
-                sample_rate,
-                use_master_clock=False,
-                include_pcs=True
-            )
+        load_result = load_kilosort_data(
+            kilosort_output_directory,
+            sample_rate,
+            use_master_clock=False,
+            include_pcs=calc_pcs
+        )
+        print("Unpacking and starting calculations..")
 
-        pc_features = None
-        pc_feature_ind = None
+        if calc_pcs:
+            spike_times, spike_clusters, spike_templates, amplitudes, templates, channel_map, clusterIDs, cluster_quality, pc_features, pc_feature_ind = load_result
+        else:
+            pc_features = None
+            pc_feature_ind = None
+            spike_times, spike_clusters, spike_templates, amplitudes, templates, channel_map, clusterIDs, cluster_quality = load_result
 
         metrics = calculate_metrics(spike_times,
                                     spike_clusters,
@@ -40,30 +54,25 @@ def run_quality_metrics(kilosort_output_directory, sample_rate, quality_metrics_
                                     pc_features,
                                     pc_feature_ind,
                                     quality_metrics_params)
-    except FileNotFoundError:
-        execution_time = time.time() - start
-        print(" Files not available.")
-        return {"execution_time": execution_time,
-                "quality_metrics_output_file": None}
-    #
-    # output_file = args['quality_metrics_params']['quality_metrics_output_file']
-    #
-    # if os.path.exists(args['waveform_metrics']['waveform_metrics_file']):
-    #     metrics = metrics.merge(pd.read_csv(args['waveform_metrics']['waveform_metrics_file'], index_col=0),
-    #                             on='cluster_id',
-    #                             suffixes=('_quality_metrics', '_waveform_metrics'))
-    #
-    # print("Saving data...")
-    # tw = 2
-    # # metrics.to_csv(output_file)
 
-    execution_time = time.time() - start
+        print('total time: ' + str(np.around(time.time() - start, 2)) + ' seconds')
+        json_data = metrics.to_json()
+        if save_to_file:
+            other_filename = f"quality_metrics_{str(uuid.uuid4())}"
+            try:
+                print(f"Saving metrics to file '{save_to_file}'")
+                fp = open(save_to_file, "w")
+                fp.write(json_data)
+                fp.close()
+            except Exception as e:
+                print(f"Error saving metrics to specified file '{save_to_file}'! Saving to file '{other_filename}' \nError: {str(e)}")
+                fp = open(other_filename, "w")
+                fp.write(json_data)
+                fp.close()
 
-    print('total time: ' + str(np.around(execution_time, 2)) + ' seconds')
-    print()
-
-    return {"execution_time": execution_time,
-            "quality_metrics_output_file": "adlfkjasasdf"}  # output manifest
+        return json.loads(json_data)
+    except FileNotFoundError as e:
+        raise FileNotFoundError(f"Cannot find file needed to run quality metrics! Error: {str(e)}")
 
 
 def generate_input_json():
@@ -102,7 +111,9 @@ def main():
             "drift_metrics_interval_s": 51,
             "drift_metrics_min_spikes_per_interval": 10,
             "include_pc_metrics": True
-        }
+        },
+        save_to_file="quality_metrics.json",
+        calc_pcs=False
     )
 
     tw = 2
