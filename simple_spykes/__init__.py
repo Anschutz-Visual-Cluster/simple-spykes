7f7eb634731cd0682daa0a9e154becd99f7f85c2
Spencer H <SpencerHanson3141@gmail.com>
2023-08-01 17:24:03 -0600

    ver 0.0.2

--- simple_spykes/__init__.py
+++ simple_spykes/__init__.py
@@ -1,3 +1,3 @@
-from ecephys_spike_sorting.scripts.create_input_json import createInputJson
-
-print("Package in development")
+# Suppress Git error when importing from ecephys, so it doesn't throw an exception
+import os
+os.environ["GIT_PYTHON_REFRESH"] = "quiet"
