5e373fa3b7aecb595a0ffd6c8df7f8a7bc42c2a4
Spencer H <SpencerHanson3141@gmail.com>
2024-05-01 18:04:29 -0600

    Version 0.1.0 published

--- simple_spykes/metric_packages/ecephys_metrics.py
+++ simple_spykes/metric_packages/ecephys_metrics.py
@@ -69,6 +69,28 @@ def ecephys_run_quality_metrics(kilosort_output_directory, sample_rate, quality_
         pc_feature_ind = None
         spike_times, spike_clusters, spike_templates, amplitudes, templates, channel_map, clusterIDs, cluster_quality = load_result
 
+        # PC Metrics are disabled as they consistently fail to run / aren't efficient
+        # # Pc metrics isolation distance, l_ratio, d_primt, nn_hit_rate, nn_mis_rate
+        # "num_channels_to_compare": 7,
+        # "max_spikes_for_unit": 500,
+        # "max_spikes_for_nn": 10000,
+        # "n_neighbors": 4,
+
+        # # Silhouette score
+        # 'n_silhouette': 10000,
+        # # Drift metrics (max drift, cumulative drift)
+        # "drift_metrics_interval_s": 51,
+        # "drift_metrics_min_spikes_per_interval": 10
+
+        # Including Doc example in here in case we ever re-enable PC metrics
+        # "num_channels_to_compare": 7,
+        # "max_spikes_for_unit": 500,
+        # "max_spikes_for_nn": 10000,
+        # "n_neighbors": 4,
+        # 'n_silhouette': 10000,
+        # "drift_metrics_interval_s": 51,
+        # "drift_metrics_min_spikes_per_interval": 10
+
         metrics = calculate_metrics(spike_times,
                                     spike_clusters,
                                     spike_templates,
--- tests/ecephys_testing.py
+++ tests/ecephys_testing.py
@@ -5,36 +5,17 @@ from simple_spykes.metric_packages.ecephys_metrics import ecephys_run_quality_me
 def ecephys_testing():
     os.chdir("metric_data")
     result = ecephys_run_quality_metrics(
-        # Path to the output of kilosort, folder containing continuous.dat
-        "..\\..\\data\\Record Node 105\\experiment1\\recording1\\continuous\\Neuropix-PXI-104.ProbeA-AP",
-        # float (optional) AP band sample rate in Hz, defaults to 30000
+        # Path to the output of kilosort, folder does NOT need to contain continuous.dat
+        "E:\\QualityMetrics\\datasets\\josh\\curated\\2023-04-13",
+        # float AP band sample rate in Hz, defaults to 30000
         30000.0,
         {
             "isi_threshold": 0.0015,
             "min_isi": 0.000166,
-
-            # PC Metrics are disabled as they consistently fail to run / aren't efficient
-            # # Pc metrics isolation distance, l_ratio, d_primt, nn_hit_rate, nn_mis_rate
-            # "num_channels_to_compare": 7,
-            # "max_spikes_for_unit": 500,
-            # "max_spikes_for_nn": 10000,
-            # "n_neighbors": 4,
-
-            # # Silhouette score
-            # 'n_silhouette': 10000,
-            # # Drift metrics (max drift, cumulative drift)
-            # "drift_metrics_interval_s": 51,
-            # "drift_metrics_min_spikes_per_interval": 10
-
-            # Including Doc example in here in case we ever re-enable PC metrics
-            # "num_channels_to_compare": 7,
-            # "max_spikes_for_unit": 500,
-            # "max_spikes_for_nn": 10000,
-            # "n_neighbors": 4,
-            # 'n_silhouette': 10000,
-            # "drift_metrics_interval_s": 51,
-            # "drift_metrics_min_spikes_per_interval": 10
-
         },
-        save_to_file="quality_metrics.json"
+        save_filename="ecephys_quality_metrics.json"
     )
+
+
+# if __name__ == "__main__":
+#     ecephys_testing()
